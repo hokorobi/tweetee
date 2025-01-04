5fed05d07dda5d4f301ba21a9fdfa8ffbfc4137e
hokorobi <hokorobi.hokorobi@gmail.com>
2025-01-04 16:49:17 +0900

    Fix build

--- build_release.bat
+++ build_release.bat
@@ -1,3 +1,16 @@
+cd tweet
+go vet
+for %%j in (shadow, defers) do for /f %%i in ('where %%j') do go vet --vettool=%%i
+cd ..\cmd\tweet-bsky
 go vet
 for %%j in (shadow, defers) do for /f %%i in ('where %%j') do go vet --vettool=%%i
 go build -ldflags "-s -H=windowsgui"
+cd ..\tweet-changelog
+go vet
+for %%j in (shadow, defers) do for /f %%i in ('where %%j') do go vet --vettool=%%i
+go build -ldflags "-s -H=windowsgui"
+cd ..\tweetee
+go vet
+for %%j in (shadow, defers) do for /f %%i in ('where %%j') do go vet --vettool=%%i
+go build -ldflags "-s -H=windowsgui"
+cd ..\..
