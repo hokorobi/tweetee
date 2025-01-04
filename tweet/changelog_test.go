666741628af1f5946fee58d0ac16c80e0c1ae752
hokorobi <hokorobi.hokorobi@gmail.com>
2025-01-04 16:49:28 +0900

    Fix test

--- tweet/changelog_test.go
+++ tweet/changelog_test.go
@@ -29,21 +29,19 @@ func TestMatchDate(t *testing.T) {
 func TestGetTweet(t *testing.T) {
 	n := time.Now().Format("15:04")
 	cases := []struct {
-		args   []string
+		args   string
 		answer []string
 	}{
-		{[]string{"test"}, []string{fmt.Sprintf("\t%s test", n), ""}},
-		{[]string{"test test"}, []string{fmt.Sprintf("\t%s \"test test\"", n), ""}},
-		{[]string{"test", "test"}, []string{fmt.Sprintf("\t%s test test", n), ""}},
-		{[]string{"a b", "c", "d"}, []string{fmt.Sprintf("\t%s \"a b\" c d", n), ""}},
-		{[]string{"a\nb"}, []string{fmt.Sprintf("\t%s a", n), "\tb", ""}},
+		{" test", []string{fmt.Sprintf("\t%s test", n), ""}},
+		{" test test", []string{fmt.Sprintf("\t%s test test", n), ""}},
+		{" a\nb", []string{fmt.Sprintf("\t%s a", n), "\tb", ""}},
 		// {[]string{"a \n b"}, []string{fmt.Sprintf("\t%s a", n), "\tb", ""}},                               // 実際の動きと違う？
 		// {[]string{"test test\ntest test"}, []string{fmt.Sprintf("\t%s test test", n), "\ttest test", ""}}, // 実際の動きと違う？
 	}
 
 	for _, c := range cases {
-		if !reflect.DeepEqual(getTweet(genText(c.args)), c.answer) {
-			t.Errorf("date: %s, result: %s, answer: %s", c.args, getTweet(genText(c.args)), c.answer)
+		if !reflect.DeepEqual(getTweet(c.args), c.answer) {
+			t.Errorf("date: %s, result: %s, answer: %s", c.args, getTweet(c.args), c.answer)
 		}
 	}
 }
